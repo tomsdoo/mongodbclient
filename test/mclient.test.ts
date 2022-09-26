b7c0ecc2e7267659f453bed12231bbeb00871bde
tom <toms@live.jp>
2022-09-26 13:58:54 +0900

    test for upsert() is added

--- test/mclient.test.ts
+++ test/mclient.test.ts
@@ -1,6 +1,7 @@
 import { describe, it } from "mocha";
 import { MClient } from "../src/mongodbclient";
 import { strict as assert } from "assert";
+import { mock } from "sinon";
 
 const testConfig = {
   uri: "mongodb+srv://...",
@@ -24,5 +25,36 @@ describe("MClient", () => {
 
   it("property collection", () => {
     assert.equal(mdbc.collection, testConfig.collection);
+  });
+
+  it("upsert()", async () => {
+    const connection = {
+      collection: {
+        updateOne: async (obj: any) =>
+          await Promise.resolve({
+            upsertedCount: 1,
+            modifiedCount: 0,
+          }),
+      },
+      client: {
+        close: () => undefined,
+      },
+    };
+    const mocked = mock(mdbc);
+    mocked
+      .expects("getConnected")
+      .once()
+      .withArgs()
+      .returns(Promise.resolve(connection));
+
+    assert.equal(
+      await mdbc
+        .upsert({ name: "test" })
+        .then(({ upsertedCount }) => upsertedCount),
+      1
+    );
+
+    mocked.verify();
+    mocked.restore();
   });
 });
