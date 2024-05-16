bf75b4f00f4feed69b0914dda36a32c30d842770
tom <toms@live.jp>
2024-05-16 19:45:45 +0900

    tsup

---
+++ tsup.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/mongodbclient.ts"],
+  clean: true,
+  format: ["esm","cjs"],
+  outExtension({ format }) {
+    return {
+      js: `.${format}.js`,
+    };
+  },
+});
