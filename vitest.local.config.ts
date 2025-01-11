4d7fe95412587486b66e6286a079ede3a17aba19
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2025-01-11 12:39:07 +0900

    lint fix (#79)

--- vitest.local.config.ts
+++ vitest.local.config.ts
@@ -1,7 +1,7 @@
+import { fileURLToPath } from "url";
+import { loadEnvFile } from "process";
 import { defineConfig } from "vitest/config";
 import defaultConfig from "./vitest.config";
-import { loadEnvFile } from "process";
-import { fileURLToPath } from "url";
 
 loadEnvFile(fileURLToPath(new URL("./.env.local", import.meta.url)));
 
